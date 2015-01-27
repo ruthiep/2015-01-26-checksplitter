81e47fd5072bfc4c3f15ffdd395b06823a6c8f3d
Ruthie Peters <ruthie_peters@yahoo.com>
2015-01-27 11:59:35 -0600

    DinnerClub calls checksplitter. need to change so it stores for each person in the hash.

---
+++ checksplitter3.rb
@@ -0,0 +1,71 @@
+"require pry"
+
+
+class Checksplitter
+
+  def initialize 
+    @party = 0
+    @total = 0
+    @tip = 1.2
+  end
+  
+  def calc_tip 
+   puts "do you want to change the tip amount? Y/N"
+   answer = gets.chomp.upcase
+   if answer == "Y"
+     puts "what amount would you like?"
+     @tip = gets.chomp.to_f
+     if @tip <= 0 
+       @tip = 1
+     else @tip += 1
+     end
+   end
+   @tip
+ end
+     
+   def set_party
+     puts "how many in your party"
+     @party = gets.chomp.to_i
+   end
+     
+   def set_bill
+     puts "how much was the bill"
+     @total = gets.chomp.to_i
+   end
+   
+   def total_bill
+     total_bill = calc_tip * set_bill 
+   end
+   
+  def amount_due
+    amt_due = total_bill / set_party
+    amt_due.to_s
+  end
+    
+ 
+end
+
+
+class DinnerClub
+  
+  def initialize
+    @diners = {Bob: 20, Joan: 25, Ruthie: 20}
+    
+  end
+
+  today = Checksplitter.new
+  
+  puts "you each owe $" + today.amount_due    
+  
+end
+
+test = DinnerClub.new
+
+
+  
+  
+ 
+    
+  
+  
+  
