aa9c91b2d99a44551c81b3c3a377b61a43716523
Ruthie Peters <ruthie_peters@yahoo.com>
2015-01-27 17:46:16 -0600

    The club history method works...shows restaurant and who went.

--- checksplitter3.rb
+++ checksplitter3.rb
@@ -10,22 +10,11 @@ class Checksplitter
   end
   
   def calc_tip 
-   puts "do you want to change the tip amount? Y/N"
-   answer = gets.chomp.upcase
-   if answer == "Y"
-     puts "what amount would you like?"
-     @tip = gets.chomp.to_f
-     if @tip <= 0 
-       @tip = 1
-     else @tip += 1
-     end
-   end
    @tip
  end
      
    def set_party
-     puts "how many in your party"
-     @party = gets.chomp.to_i
+     @party = test.add_diners.length
    end
      
    def set_bill
@@ -34,47 +23,77 @@ class Checksplitter
    end
    
    def total_bill
-     total_bill = calc_tip * set_bill 
+     calc_tip * set_bill 
    end
    
   def amount_due
-    amt_due = total_bill / set_party
+    total_bill / set_party
   end
     
- 
 end
 
 
 class DinnerClub
   
   def initialize
-    @diners = {Bob: 20, Joan: 25, Ruthie: 20}
+    @diners = []
+    @balance_sheet = {}
+    @dinner_log = []
+    @club_history = []
     
   end
   
-  def update_balance
-    today = Checksplitter.new
-    today.amount_due
+  def add_diners
+    answer = "Y"
+    while answer == "Y"
+      puts "Who is eating today with the club?"
+      answer = gets.chomp
+      @diners << answer
+      puts "Any more? Y/N"
+      answer = gets.chomp.upcase
+    end   
   end
   
-  def add_to_total
-    add_amt = update_balance
-    @diners.each do |name, amount|
-      @diners[name] += add_amt
-    end
-     puts @diners 
+  
+  def update_history 
+    puts "which restaurant?"
+    restaurant = gets.chomp
+    add_diners
+    @club_history << restaurant << @diners
+    puts @club_history
   end
+  
+  def check_if_splitting
+    puts "Is everyone paying today?"
+    answer = gets.chomp.upcase
+    if answer == "Y"
+      return true
+    else
+      return false
+    end
+  end
+  
+  def amount_due
+    if check_if_splitting
+      today = Checksplitter.new  
+      today.amount_due
+    end
+  end
+  
+  # def add_to_total
+#     add_amt = amount_due
+#     @diners.each do |name, amount|
+#       @balance_sheet[@diners] += add_amt
+#     end
+#      puts @balance_sheet
+#   end
   
 end
 
 test = DinnerClub.new
 
-test.add_to_total
-
-  
-  
- 
-    
-  
+# test.add_diners
+# test.add_to_total
+test.update_history
   
   
