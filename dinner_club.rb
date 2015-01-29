dbc6ce57bcf46565fc30a40bce85020b077fd0ce
Ruthie Peters <ruthie_peters@yahoo.com>
2015-01-28 22:16:33 -0600

    add commenting formats

--- dinner_club.rb
+++ dinner_club.rb
@@ -8,7 +8,23 @@ require_relative 'person'
 #track outings
 #let members treat
 
-
+# Class: DinnerClub
+#
+# Tracks who attended each dinner and their running balance.
+#
+# Attributes:
+# @diners          - Array: the names of the people who attended that dinner.
+# @balance_sheet   - Hash: the names and how much they owe for that dinner. 
+# @club_history    - Array: the history of each person's balance.
+#
+# Public Methods:
+# #add_diners
+# #update_history
+# #format_history
+# #check_if_splitting
+# #calc_portion_of_bill
+# #treats
+# #add_to_total
 
 class DinnerClub
   
@@ -19,11 +35,39 @@ class DinnerClub
     @events = {}
   end
 
-
+  # Public: #add_diners
+  # Adds the diners to the array.
+  #
+  # Parameters:
+  # answer   - String: name input from user.
+  # @diners  - Array:  the array to be populated.
+  #
+  #
+  # Returns:
+  # @diners: The completed array.
+  #
+  # State changes:
+  # adds to @diners
+   
   def add_member( member )
     @members[member] = Person.new ( member )
     @members
   end
+
+  # Public: #add_diners
+  # Adds the diners to the array.
+  #
+  # Parameters:
+  # answer   - String: name input from user.
+  # @diners  - Array:  the array to be populated.
+  #
+  #
+  # Returns:
+  # @diners: The completed array.
+  #
+  # State changes:
+  # adds to @diners
+   
 
   def show_members
     @members.each do |member, objs|
@@ -32,10 +76,39 @@ class DinnerClub
     @members
   end
   
+  # Public: #add_diners
+  # Adds the diners to the array.
+  #
+  # Parameters:
+  # answer   - String: name input from user.
+  # @diners  - Array:  the array to be populated.
+  #
+  #
+  # Returns:
+  # @diners: The completed array.
+  #
+  # State changes:
+  # adds to @diners
+   
+  
   def remove_member( member )
     @members.delete(member)
     @members
   end
+  # Public: #add_diners
+  # Adds the diners to the array.
+  #
+  # Parameters:
+  # answer   - String: name input from user.
+  # @diners  - Array:  the array to be populated.
+  #
+  #
+  # Returns:
+  # @diners: The completed array.
+  #
+  # State changes:
+  # adds to @diners
+   
   
   def have_an_outing( total, tip, *diners )
     cs = CheckSplitter.new( total, tip, diners.length )
@@ -54,14 +127,57 @@ class DinnerClub
     @members
   end
   
+  # Public: #add_diners
+  # Adds the diners to the array.
+  #
+  # Parameters:
+  # answer   - String: name input from user.
+  # @diners  - Array:  the array to be populated.
+  #
+  #
+  # Returns:
+  # @diners: The completed array.
+  #
+  # State changes:
+  # adds to @diners
+   
   def get_spending_report( member )
     @members[member]
   end
+  
+  # Public: #add_diners
+  # Adds the diners to the array.
+  #
+  # Parameters:
+  # answer   - String: name input from user.
+  # @diners  - Array:  the array to be populated.
+  #
+  #
+  # Returns:
+  # @diners: The completed array.
+  #
+  # State changes:
+  # adds to @diners
+   
   
   def add_event( restaurant, *member )
     @events[restaurant] = *member
     puts @events
   end
+  # Public: #add_diners
+  # Adds the diners to the array.
+  #
+  # Parameters:
+  # answer   - String: name input from user.
+  # @diners  - Array:  the array to be populated.
+  #
+  #
+  # Returns:
+  # @diners: The completed array.
+  #
+  # State changes:
+  # adds to @diners
+   
   
   def treat (member, total)
      # add_event( restaurant, *members ) 
--- person.rb
+++ person.rb
@@ -1,3 +1,23 @@
+# Class: DinnerClub
+#
+# Tracks who attended each dinner and their running balance.
+#
+# Attributes:
+# @diners          - Array: the names of the people who attended that dinner.
+# @balance_sheet   - Hash: the names and how much they owe for that dinner. 
+# @club_history    - Array: the history of each person's balance.
+#
+# Public Methods:
+# #add_diners
+# #update_history
+# #format_history
+# #check_if_splitting
+# #calc_portion_of_bill
+# #treats
+# #add_to_total
+
+
+
 class Person
   
   attr_accessor :name, :spending_amount
@@ -9,6 +29,20 @@ class Person
     
   end
   
+  # Public: #add_diners
+  # Adds the diners to the array.
+  #
+  # Parameters:
+  # answer   - String: name input from user.
+  # @diners  - Array:  the array to be populated.
+  #
+  #
+  # Returns:
+  # @diners: The completed array.
+  #
+  # State changes:
+  # adds to @diners
+   
   def spend( amt )
     @spending_amount += amt
   end
